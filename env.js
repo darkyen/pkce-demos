f4f8ca69b571a1edcceff467710bcb117805218c
Abhishek Hingnikar <abhishek@hingnikar.com>
2017-02-09 19:40:11 -0500

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
 module.exports = {
+    /* These are all the secrets :D */
     domain: 'some-game.auth0.com',
     clientID: '89U7uQnK4wXYhB9dLUXSRDsdyTZRANTb',
     packageIdentifier: 'com.auth0.example.cordova',
