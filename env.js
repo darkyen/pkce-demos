5dcd1d4512180fbf2eea0ac0283250686a9500af
Abhishek Hingnikar <abhishek@hingnikar.com>
2017-02-09 19:41:16 -0500

    Remove shared secret! 🙆🏻

--- env.js
+++ env.js
@@ -3,5 +3,5 @@ module.exports = {
     domain: 'some-game.auth0.com',
     clientID: '89U7uQnK4wXYhB9dLUXSRDsdyTZRANTb',
     packageIdentifier: 'com.auth0.example.cordova',
-    audience: 'http://localhost:3000/anny'
+    audience: 'http://some-game.auth0.com/userinfo'
 }
