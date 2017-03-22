54d8eb3523929340d7e1b411fe956a182610c38a
Just a random person <abhishek@hingnikar.com>
2017-03-22 11:44:05 -0700

    Remove example

--- env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    /* These are all the secrets :D */
-    domain: 'some-game.auth0.com',
-    clientID: '89U7uQnK4wXYhB9dLUXSRDsdyTZRANTb',
-    packageIdentifier: 'com.auth0.example.cordova',
-    audience: 'http://some-game.auth0.com/userinfo'
-}
